e6a0080c89553bca1e2eae49f4e129a55554842a
Sergio <sergiorc2000@hotmail.es>
2020-06-28 11:35:02 -0600

    Chest v1

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, Interactable
+{
+    [SerializeField]
+
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+
+    private Sprite openSprite, closeSprite;
+    bool isOpen;
+    public void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Interact();
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StopInteract();
+        }
+    }
+    public void Interact()
+    {
+        if (isOpen)
+        {
+            StopInteract();
+        }
+        else
+        {
+            isOpen = true;
+            spriteRenderer.sprite = openSprite;
+        }
+
+    }
+    public void StopInteract()
+    {
+        spriteRenderer.sprite = closeSprite;
+    }
+}

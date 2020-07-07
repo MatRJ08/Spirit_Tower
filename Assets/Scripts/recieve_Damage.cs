7454a6f4ffd0bf1a27075a24302dbbe5e9c5f8ce
Sergio <sergiorc2000@hotmail.es>
2020-07-07 17:38:47 -0600

    Vidas de jugador v0.1

--- Assets/Scripts/recieve_Damage.cs
+++ Assets/Scripts/recieve_Damage.cs
@@ -3,24 +3,41 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class recieve_Damage : MonoBehaviour
 {
+    public Transform[] enemies;
     public float health;
     public float maxHealth;
+    private int numHearts= 5;
+    public Image[] hearts;
+    public Sprite full;
+    public Sprite empty;
+
     void Start()
     {
+        print("NUM start: " + numHearts);
         health = maxHealth;
-        
     }
+   
     public void DealDamage(float damage)
     {
+        
+     
+        for (int i = 0; i <numHearts; i++)
+        {
+            if (i < numHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
 
-       //print("damage: " + damage);
-        //Client.instance.SendData("REQUEST|PLAYER|DAMAGE:" + Math.Round(damage));
-
-       
-       // Client.instance.SendData("REQUEST|PLAYER|DAMAGE:" + Math.Round(damage));
+        // Client.instance.SendData("REQUEST|PLAYER|DAMAGE:" + Math.Round(damage));
 
         health -= damage;
         CheckDeath();
@@ -37,7 +54,23 @@ public class recieve_Damage : MonoBehaviour
     {
         if (health <= 0)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            numHearts -= 1;
+            print("NUM death: " + numHearts);
+            if (numHearts == 0)
+            {
+                print("DEST");
+                Destroy(gameObject);
+            }
+            else
+            {
+                print("SCENE");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+               /* for (int i = 0; i < enemies.Length; i++)
+                {
+                    enemies[i].position=GetComponent
+                }*/
+            }
         }
     }
 }
